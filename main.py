6223b9d6a3ebc1e0e7ea0caa099a180fa33bb247
Ben Green <17412026+Pathfinder216@users.noreply.github.com>
2020-01-25 21:07:28 -0500

    Setup resource system and material style

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import sys
 from PyQt5.QtQml import QQmlApplicationEngine
 from PyQt5.QtWidgets import QApplication
 
+import resources
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
