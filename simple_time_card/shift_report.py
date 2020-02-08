5a97fb3ee959792cb5473e00ecbdcc0382b89d66
Ben Green <17412026+Pathfinder216@users.noreply.github.com>
2020-02-08 16:39:23 -0500

    Add UI-accessible date and length to ShiftReport

--- simple_time_card/shift_report.py
+++ simple_time_card/shift_report.py
@@ -1,8 +1,29 @@
 from dataclasses import dataclass
 from datetime import datetime
 
+from PyQt5.QtCore import QObject, pyqtProperty
+from PyQt5.QtQml import qmlRegisterType
+
 
 @dataclass
-class ShiftReport:
+class ShiftReport(QObject):
     start: datetime
     end: datetime
+
+    def __post_init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @pyqtProperty(str, constant=True)
+    def date(self):
+        local_start = self.start.astimezone(tz=None)
+        return str(local_start.date())
+
+    @pyqtProperty(str, constant=True)
+    def length(self):
+        time_delta = self.end - self.start
+        hours, remainder = divmod(time_delta.seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        return f"{hours}:{minutes:02d}:{seconds:02d}"
+
+
+qmlRegisterType(ShiftReport)
