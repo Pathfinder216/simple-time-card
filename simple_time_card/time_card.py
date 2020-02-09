3c75fe302d251e0946c49064fc1adeb68710e717
Ben Green <17412026+Pathfinder216@users.noreply.github.com>
2020-02-08 19:36:58 -0500

    Add TimeCard JSON encoder and decoder

--- simple_time_card/time_card.py
+++ simple_time_card/time_card.py
@@ -1,3 +1,5 @@
+import json
+import os
 from dataclasses import dataclass, field
 from datetime import datetime, timezone
 from typing import List
@@ -6,6 +8,43 @@ from PyQt5.QtCore import QObject, pyqtProperty, pyqtSignal, pyqtSlot
 
 from .minute_timer import MinuteTimer
 from .shift_report import ShiftReport
+
+
+class TimeCardEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime):
+            return {"__type__": datetime.__name__, "isoformat": obj.isoformat()}
+
+        if isinstance(obj, (ShiftReport, TimeCard)):
+            object_data = vars(obj)
+            object_data["__type__"] = type(obj).__name__
+            return object_data
+
+        return super().default(obj)
+
+
+class TimeCardDecoder(json.JSONDecoder):
+    def __init__(self, *args, **kwargs):
+        super().__init__(object_hook=self.object_hook, *args, **kwargs)
+
+    def object_hook(self, serialized_data):
+        if "__type__" not in serialized_data:
+            return serialized_data
+
+        data_type = serialized_data["__type__"]
+
+        if data_type == datetime.__name__:
+            return datetime.fromisoformat(serialized_data["isoformat"])
+
+        if data_type == ShiftReport.__name__:
+            del serialized_data["__type__"]
+            return ShiftReport(**serialized_data)
+
+        if data_type == TimeCard.__name__:
+            del serialized_data["__type__"]
+            return TimeCard(**serialized_data)
+
+        raise TypeError(f"Object of type {data_type} is not JSON deserializable")
 
 
 @dataclass
